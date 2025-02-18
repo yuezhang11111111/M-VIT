d7a8285a707cd14c312700f6e4368ab00f6f64f7
yuezhang11111111 <103367689+yuezhang11111111@users.noreply.github.com>
2025-02-18 20:18:09 +0800

    Add files via upload

---
+++ MVIT/visualize.py
@@ -0,0 +1,81 @@
+from tkinter import Image
+import cv2
+import numpy as np
+import tensorflow as tf
+from . import layers, mvit
+import matplotlib.pyplot as plt
+
+
+def attention_map(model, image):
+    """Get an attention map for an image and model using the technique
+    described in Appendix D.7 in the paper (unofficial).
+
+    Args:
+        model: A ViT model
+        image: An image for which we will compute the attention map.
+    """
+    img_height, img_width = model.input_shape[1], model.input_shape[2]
+    # 确保model.layers[5]不是None
+    if model.layers[5] is not None:
+       print(model.layers[6])
+    else:
+        raise ValueError("The layer at index 5 is None, cannot compute grid_size")
+    grid_size = int(np.sqrt(model.layers[5].output_shape[0][-2] - 1))
+
+    # Prepare the input
+    X = vit.preprocess_inputs(cv2.resize(image, (img_height, img_width)))[np.newaxis, :]  # type: ignore
+
+    # Get the attention weights from each transformer.
+    outputs = [
+        l.output[1] for l in model.layers if isinstance(l, layers.TransformerBlock)
+    ]
+    weights = np.array(
+        tf.keras.models.Model(inputs=model.inputs, outputs=outputs).predict(X)
+    )
+    num_layers = weights.shape[0]
+    num_heads = weights.shape[2]
+    reshaped = weights.reshape(
+        (num_layers, num_heads, grid_size**2 + 1, grid_size**2 + 1)
+    )
+
+    # From Appendix D.6 in the paper ...
+    # Average the attention weights across all heads.
+    reshaped = reshaped.mean(axis=1)
+
+    # From Section 3 in https://arxiv.org/pdf/2005.00928.pdf ...
+    # To account for residual connections, we add an identity matrix to the
+    # attention matrix and re-normalize the weights.
+    reshaped = reshaped + np.eye(reshaped.shape[1])
+    reshaped = reshaped / reshaped.sum(axis=(1, 2))[:, np.newaxis, np.newaxis]
+
+    # Recursively multiply the weight matrices
+    v = reshaped[-1]
+    for n in range(1, len(reshaped)):
+        v = np.matmul(v, reshaped[-1 - n])
+
+    # Attention from the output token to the input space.
+    mask = v[0, 1:].reshape(grid_size, grid_size)
+    mask = cv2.resize(mask / mask.max(), (image.shape[1], image.shape[0]))[
+        ..., np.newaxis
+    ]
+
+    # # 彩色注意力图
+    # #mask = cv2.applyColorMap(mask, cv2.COLORMAP_JET)
+    # #mask = (mask - mask.min()) / (mask.max() - mask.min())
+    # #mask=np.squeeze(mask)
+    # #colormap = plt.get_cmap('jet')
+    # #mask = colormap(mask)
+    # #mask = mask[:, :, :3] * 255
+    # mask=mask*255
+    # print(mask.shape)
+    # print(image.shape)
+    # mask=mask.astype(np.uint8)
+    # mask = cv2.applyColorMap(mask, cv2.COLORMAP_JET)
+    # print(mask.shape)
+    # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # #image = np.expand_dims(image, axis=-1)
+    # image=cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+    # print(image.shape)
+    # image = image.astype(np.uint8)
+    # mask_image=cv2.addWeighted(image,0.8,mask,0.2,0)
+    return (mask*image).astype("uint8")
